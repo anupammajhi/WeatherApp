fd6c08646d06e944e9a429eabeb1840153ffedca
Anupam Majhi <majhi.anupam@gmail.com>
2017-01-17 03:22:53 +0530

    Frontend Ready

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     var weatherBoxDimentions = Math.max($(".weatherBox").css("height").replace("px",""),$(".weatherBox").css("width").replace("px",""))
     $(".weatherBox").css("height",weatherBoxDimentions)
     $(".weatherBox").css("width",weatherBoxDimentions)
+
+  //Adjust Text vertical align to center
+    var contentHeight = $(".location").outerHeight() + $("#temp").outerHeight() + $("#weatherDesc").outerHeight()
+    var circleHeight = $(".weatherBox").innerHeight();
+    var ContentPaddingTop = (circleHeight - contentHeight)/2;
+    $(".location").css("padding-top",ContentPaddingTop)
 });
